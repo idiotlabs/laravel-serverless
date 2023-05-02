25154677a95096a3e9f4e0d9d138b145721f16e3
Seobs <subi88@naver.com>
2023-05-03 08:30:39 +0900

    feat: add dice privacy page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('dice')->group(function () {
+    Route::get('/agreement', [DiceController::class, 'agreement']);
+    Route::get('/privacy', [DiceController::class, 'agreement']);
 });
