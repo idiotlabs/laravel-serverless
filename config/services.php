f744c457966006f81e171f3efa444013c0035aac
Seobs <subi88@naver.com>
2024-03-18 17:32:18 +0900

    feat: add youtube service

--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'youtube-trend' => [
+        'project' => env('GOOGLE_YOUTUBE_PROJECT'),
+        'key' => env('GOOGLE_YOUTUBE_KEY'),
+    ],
 ];
